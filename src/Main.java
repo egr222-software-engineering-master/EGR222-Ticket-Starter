3149f5f4eef949dbf23d2c197e65adb146e2877d
Prof Han <mhan@calbaptist.edu>
2019-10-24 22:45:37 -0700

    initializing repo

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+    	// write your code here
+        Ticket t1 = new Ticket(12.0, 123);
+        System.out.println(t1);
+
+        Ticket t2 = new Ticket(24.0);
+        System.out.println(t2);
+
+        Ticket t3 = new Ticket(456);
+        System.out.println(t3);
+    }
+}
